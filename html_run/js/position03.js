e0cbd6480334fb1875a85fb44884c053b3681282
최은진 <105770718+chxxing@users.noreply.github.com>
2022-06-03 20:36:41 +0900

    Add files via upload

--- html_run/js/position03.js
+++ html_run/js/position03.js
@@ -2,4 +2,13 @@ $(function () {
     $('header i').on('click', function () {
         $('.cover').toggleClass('on')
     })
+    $('.cover a').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).parent().index();
+        console.log(100 * idx + "%", $('.sticky').height());
+        let h = $('.sticky').height();
+        $('.cover').removeClass('on');
+        $('html, body').animate({ scrollTop: (idx + 1) * h }, 1000)
+    })
 })
+
