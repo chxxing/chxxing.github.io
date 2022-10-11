e30a2e291dc3d1e65a5aa2615d8d56599d00a758
chxxing <choing26@naver.com>
2022-10-11 12:45:07 +0900

    221011

---
+++ src/portfolioDATA.js
@@ -0,0 +1,8 @@
+const PD = [
+    { id: 1, title: "pf01", content: "first homepage", link: "chxxing.github.io/react_soil", anchor: "pf01" },
+    { id: 2, title: "pf02", color: ['#f0f', '#ff0', '#ddd'], content: "second homepage", link: "chxxing.github.io/react_soil", anchor: "pf02" },
+    { id: 3, title: "pf03", color: ['#ddd', '#00f', '#0ff'], content: "third homepage", link: "chxxing.github.io/react_soil", anchor: "pf03" },
+    { id: 4, title: "pf04", color: ['#369', '#0f0', '#f00'], content: "fourth homepage", link: "chxxing.github.io/react_soil", anchor: "pf04" },
+]
+
+export default PD;
