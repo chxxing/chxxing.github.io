08c5f734c336dbfde80c2b90384a8cc3f9b13e76
chxxing <choing26@naver.com>
2022-10-13 17:42:43 +0900

    221013

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import ReactFullpage from '@fullpage/react-fullpage';
+import './main.scss';
+import { profile, pfdata } from './Data';
+
+const AC = pfdata.map(el => el = el.title);
 
 const App = () => {
     return (
         <div>
-            App
+            <nav className="Gnb">
+                <ul>
+                    {
+                        pfdata.map((it, idx) => {
+                            return (
+                                <li key={idx}>
+                                    <a href={`#${it.title}`}>{it.title}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+            <ReactFullpage
+                //fullpage options
+                licenseKey={'YOUR_KEY_HERE'}
+                scrollingSpeed={1000}
+                anchors={['cover', ...AC, 'profile']}
+
+                render={({ state, fullpageApi }) => {
+                    return (
+                        <ReactFullpage.Wrapper>
+                            <div className="section">
+                                <div className="case">PORTFOLIO</div>
+                            </div>
+                            {
+                                pfdata.map((it, idx) => {
+                                    return (
+                                        <div className="section" key={it.id}>
+                                            <div className="case">
+                                                <div className="preview">
+                                                    <img src="" alt="" />
+                                                </div>
+                                                <ul className="txtBox">
+                                                    <li>{it.title}</li>
+                                                    <li>{it.type}</li>
+                                                    <li>{it.info}</li>
+                                                    <li>
+                                                        <a href={it.link} target="_blank">
+                                                            SITE LINK
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div className="section">
+                                <div className="case">PROFILE</div>
+                            </div>
+                        </ReactFullpage.Wrapper>
+                    );
+                }}
+            />
         </div>
     )
 }
 
-export default App
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,14 @@
+const profile = {
+    name: "choieunjin",
+    email: "choing26@naver.com",
+    tel: "010-5167-0209"
+};
+
+const pfdata = [
+    { id: 1, title: "site01", type: "web page01", font: "Noto Sans KR", info: "0000000001", color: [], skill: [], src: "", link: "" },
+    { id: 2, title: "site02", type: "web page02", font: "Noto Sans KR", info: "0000000002", color: [], skill: [], src: "", link: "" },
+    { id: 3, title: "site03", type: "web page03", font: "Noto Sans KR", info: "0000000003", color: [], skill: [], src: "", link: "" },
+    { id: 4, title: "site04", type: "web page04", font: "Noto Sans KR", info: "0000000004", color: [], skill: [], src: "", link: "" }
+];
+
+export { profile, pfdata };
